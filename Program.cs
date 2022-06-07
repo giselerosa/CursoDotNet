458551270d5d5b9f57f8748c32c489432624ee15
Gisele Rosa <giselersantos.ti@gmail.com>
2022-06-07 15:18:23 -0300

    🎉 [Static Class] - Add a example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello World! ");
+
+int i;
+int i2 = i = 10;
+i2--;
+
+//Ternary operator
+i = i2 == 42 ? 10 : 20;
+
+Console.WriteLine(i2--);
