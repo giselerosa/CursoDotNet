b32c9f8a1c36d6a087b8780cb277d66be91c1fe0
Gisele Rosa <giselersantos.ti@gmail.com>
2022-06-08 14:33:09 -0300

    ♻️ Refactoring code using foreach

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
-﻿Console.Write("Insira o seu nome: ");
-
-string name = Console.ReadLine();
-
-Console.WriteLine($"Olá {name}!");
-Console.Write("Digite o ano do seu nascimento: ");
-
-int year = int.Parse(Console.ReadLine());
-int age = 2022 - year; // +, -, *, /
-
-Console.WriteLine($"Você tem {age} anos.");
-
-if (age >= 18) 
+﻿class Program
 {
-    Console.WriteLine("Você é maior de idade");
+    static void Main()
+    {
+        string[] names = {"Gisele", "Kaio"};
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
 }
-else 
-{
-    Console.WriteLine("Você não é maior de idade");
-} 
